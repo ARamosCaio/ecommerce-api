d6908b0856b966131d8a94f1f196689e1d8cce44
Caio Ramos <aramoscaio@gmail.com>
2023-03-24 20:53:16 -0300

    Added route to main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import mysql.connector
-from flask import Flask
+from flask import Flask, request
 
-connection = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "rootpassword",
-    database = "stock"
-)
+
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
+@app.route('/db/stock_db.sql', methods=['GET','POST','PATCH','DELETE'])
+def get_couches():
+    def list_all():
+        return self.finalize_request(rv)
+
+app.run()
