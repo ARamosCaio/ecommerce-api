dcae43d8bfa3b5ec11103bf302927c6f3201bed1
Caio Ramos <aramoscaio@gmail.com>
2023-03-28 00:07:14 -0300

    Added manager commands to generate migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy import URL
-from sqlalchemy.ext.declarative import as_declarative
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:senha123@localhost/stock'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
