3fa01a9f65391abf7e7d2c957b09649e25f9930a
Caio Ramos <aramoscaio@gmail.com>
2023-03-27 22:38:26 -0300

    Added constructors

--- app/models/tables.py
+++ app/models/tables.py
@@ -7,6 +7,12 @@ class Beds(db.Model):
     amount_stock = db.Column(db.Integer, default=0)
     price = db.Column(db.Float, nullable=True)
 
+    def __init__(self, product_type, product_size, amount_stock, price):
+        self.product_type = product_type
+        self.product_size = product_size
+        self.amount_stock = amount_stock
+        self.price = price
+
     def __repr__(self):
         return '<Beds %r %r %r %r>' % self.product_type, self.product_size, self.amount_stock, self.price
 
@@ -17,8 +23,15 @@ class Couches(db.Model):
     amount_stock = db.Column(db.Integer, default=0)
     price = db.Column(db.Float, nullable=True)
 
+    def __init__(self, product_type, product_size, amount_stock, price):
+        self.product_type = product_type
+        self.product_size = product_size
+        self.amount_stock = amount_stock
+        self.price = price
+
     def __repr__(self):
         return '<Couches %r %r %r %r>' % self.product_type, self.product_size, self.amount_stock, self.price
+
 
 class Wardrobe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,6 +39,12 @@ class Wardrobe(db.Model):
     product_size = db.Column(db.String, nullable=False)
     amount_stock = db.Column(db.Integer, default=0)
     price = db.Column(db.Float, nullable=True)
+
+    def __init__(self, product_type, product_size, amount_stock, price):
+        self.product_type = product_type
+        self.product_size = product_size
+        self.amount_stock = amount_stock
+        self.price = price
 
     def __repr__(self):
         return '<Wardrobe %r %r %r %r>' % self.product_type, self.product_size, self.amount_stock, self.price
