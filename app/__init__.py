ed1bea3ae5e436f3800e7e42d6f104c0ff5a86d5
Caio Ramos <aramoscaio@gmail.com>
2023-03-28 22:13:03 -0300

    Changed the migrations script removing the flask_script

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
