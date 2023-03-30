1f9774d620529456a16f482eb92e12dc4090040a
Caio Ramos <aramoscaio@gmail.com>
2023-03-29 21:27:28 -0300

    Updated __init__.py script to make the migrations using flask.cli

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from app.db import db
+from flask_migrate import Migrate
+import app.models 
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
 
+@app.route("/beds", methods=['GET'])
+def get_beds():
+    return jsonify({"beds-sucess"})
+
+@app.route("/wardrobe", methods=['GET'])
+def get_wardrobes():
+    return jsonify({"wardrobe-sucess": "item"})
+
+@app.route("/couches", methods=['GET'])
+def get_couches():
+    return jsonify({"couches-sucess"})
